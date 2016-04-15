b60932dd5e44f364634bc6e852a2141cd55a9566
Jamie Penney <jamie@jamiepenney.co.nz>
2016-04-15 22:12:36 +1200

    Setup gulp

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+var autoprefixerOptions = {
+  browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+};
+
+var sassdocOptions = {
+  dest: './sassdoc'
+};
+
+var sass = require('gulp-sass');
+gulp.task('sass', function () {
+  gulp.src('./sass/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'sass:watch']);
